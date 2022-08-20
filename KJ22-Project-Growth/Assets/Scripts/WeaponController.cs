381536a5c0fe7e2737d9ae92099c3ae088838eef
Morgan Hoarau <morgansb.hoarau@gmail.com>
2022-08-20 14:51:26 +0100

    Added start bullet work

--- KJ22-Project-Growth/Assets/Scripts/WeaponController.cs
+++ KJ22-Project-Growth/Assets/Scripts/WeaponController.cs
@@ -1,9 +1,11 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponController : MonoBehaviour
 {
+	// [SerializeField, NaughtyAttributes.Required("Missing m_PlayerTransform.")]
+	// private Transform m_PlayerTransform;
     [SerializeField, NaughtyAttributes.Required("Missing m_cursorPivotTransform.")]
     private Transform m_cursorPivotTransform;
     // Start is called before the first frame update
@@ -14,10 +16,10 @@ public class WeaponController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+	{
         Vector3 MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        m_cursorPivotTransform.up = new Vector2(MousePosition.x - transform.position.x,
-            MousePosition.y - transform.position.y).normalized;
+        m_cursorPivotTransform.up = new Vector2(MousePosition.x - m_cursorPivotTransform.position.x,
+            MousePosition.y - m_cursorPivotTransform.position.y).normalized;
     }
 }
