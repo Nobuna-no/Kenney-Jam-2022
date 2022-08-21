9bb09c8573b6359a150a90ba7a50d6b98b270915
marckes04 <maresp95@gmail.com>
2022-08-21 00:29:23 -0500

    first commit

---
+++ KJ22-Project-Growth/Assets/Scripts/MenuController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Levels");
+    }
+
+    public void Instructions()
+    {
+        SceneManager.LoadScene("Instructions");
+    }
+
+    public void Level1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Level2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+    public void Level3()
+    {
+        SceneManager.LoadScene("Level3");
+    }
+
+    public void Level4()
+    {
+        SceneManager.LoadScene("Level4");
+    }
+
+    public void Level5()
+    {
+        SceneManager.LoadScene("Level5");
+    }
+    public void Level6()
+    {
+        SceneManager.LoadScene("Level6");
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
